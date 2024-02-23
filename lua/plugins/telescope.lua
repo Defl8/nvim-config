8dd8e427d9f07e45d2bbb6004e7929330d3b262a
Wyatt James <wyattsamueljames@gmail.com>
2024-02-23 00:26:39 -0700

    feat: added telescope, treesitter, oil, leap and fugitive

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    lazy = true,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+        { "<leader>ff", "<CMD>Telescope find_files<CR>"},
+        { "<leader>fg", "<CMD>Telescope live_grep<CR>" },
+    },
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      require("telescope").load_extension("ui-select")
+    end,
+  },
+}
