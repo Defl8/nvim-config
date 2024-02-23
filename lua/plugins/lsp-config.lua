bfe2d79f05d1c89e8e779d29fec0c5cfe89fe9c6
Wyatt James <wyattsamueljames@gmail.com>
2024-02-23 07:44:53 -0700

    feat: added lsp and completions

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason", -- Load when :Mason is used
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    event = "VeryLazy", -- Loads after critical plugins
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" }, -- Load when new buffer is created or opened
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      local lspconfig = require("lspconfig")
+      lspconfig.tsserver.setup({
+        capabilities = capabilities
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
