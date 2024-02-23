d9f34ac2d98e6f830bbab09740fde51f40f6a405
Wyatt James <wyattsamueljames@gmail.com>
2024-02-23 12:56:14 -0700

    feat(lsp): added marksman

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -32,6 +32,9 @@ return {
       lspconfig.pyright.setup({
         capabilities = capabilities
       })
+      lspconfig.marksman.setup({
+        capabilities = capabilities
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
