0deb066c454629bc32a9f6e371f5d1811ceb5fc6
Wyatt James <wyattsamueljames@gmail.com>
2024-02-27 11:02:36 -0700

    feat: added harpoon config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -32,6 +32,12 @@ return {
       lspconfig.marksman.setup{
         capabilities = capabilities
       }
+      lspconfig.cssls.setup{
+        capabilities = capabilities
+      }
+      lspconfig.tsserver.setup{
+        capabilities = capabilities
+      }
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
