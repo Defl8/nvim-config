f889c8def205c56c95963fe3bfa96d1a42f2702e
Wyatt James <wyattsamueljames@gmail.com>
2024-02-27 09:48:17 -0700

    feat(obisidian.lua): added template

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -11,6 +11,13 @@ return {
                     name = "codecademy-lua",
                     path = "C:/Users/wyatt/Documents/github/codecademy-lua",
                 },
+                {
+                    name = "Semester 2",
+                    path = "C:/Users/wyatt/Documents/github/CNT-Obsidian-Notes/Semester 2",
+                    templates = {
+                        subdir = "Templates"
+                    }
+                },
             }
         })
     end,
