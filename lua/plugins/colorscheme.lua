cf1c9d62c49ffac18a954845e64d82a922644d72
Wyatt James <wyattsamueljames@gmail.com>
2024-03-01 18:21:37 -0700

    refactor(colorscheme): onedark

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "navarasu/onedark.nvim",
+        name = "onedark",
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme "onedark"
+            require('onedark').setup {
+                style = "deep",
+            }
+            require('onedark').load()
+        end
+    }
+}
