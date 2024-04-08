0a1c32e3d7af6e0d084659294e51f9a8dc187df5
Wyatt James <wyattsamueljames@gmail.com>
2024-04-08 08:33:37 -0600

    feat(lsp): python lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -47,6 +47,9 @@ return {
       lspconfig.sqlls.setup{
         capabilities = capabilities
       }
+      lspconfig.pylsp.setup{
+        capabilities = capabilities
+      }
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
