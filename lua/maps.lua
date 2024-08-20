14eaa7073cddd93adccfb24a004db4f13d1a364c
Wyatt James <wyattsamueljames@gmail.com>
2024-08-20 08:27:07 -0600

    feat: add new maps

--- lua/maps.lua
+++ lua/maps.lua
@@ -7,3 +7,17 @@ vim.g.mapleader = ' '
 -- Page up and down center cursor
 map('n', '<C-d>', '<C-d>zz', {noremap = true, silent = true})
 map('n', '<C-u>', '<C-u>zz', {noremap = true, silent = true})
+
+-- Next searched word center cursor
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+-- Paste from system clipboard register
+map("x", "<leader>p", [["_dP]])
+
+-- Yank to system clipboard register
+map({"n", "v"}, "<leader>y", [["+y]])
+map("n", "<leader>Y", [["+Y]])
+
+-- Delete to system clipboard register
+map({"n", "v"}, "<leader>d", [["_d]])
