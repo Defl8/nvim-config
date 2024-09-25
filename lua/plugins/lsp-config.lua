4d649f4a8c5489fc5b5b828d447ab2ac307654ec
Wyatt James <wyattsamueljames@gmail.com>
2024-09-25 09:13:34 -0600

    feat: add sqlls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,53 +1,56 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    event = "VeryLazy", -- Loads after critical plugins
-    opts = {
-      auto_install = true,
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
     },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" }, -- Load when new buffer is created or opened
-    config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    {
+        "williamboman/mason-lspconfig.nvim",
+        event = "VeryLazy", -- Loads after critical plugins
+        opts = {
+            auto_install = true,
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" }, -- Load when new buffer is created or opened
+        config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-      local lspconfig = require("lspconfig")
+            local lspconfig = require("lspconfig")
 
-      lspconfig.html.setup{
-        capabilities = capabilities
-      }
-      lspconfig.lua_ls.setup{
-        capabilities = capabilities
-      }
-      lspconfig.marksman.setup{
-        capabilities = capabilities
-      }
-      lspconfig.cssls.setup{
-        capabilities = capabilities
-      }
-      lspconfig.ts_ls.setup{
-        capabilities = capabilities
-      }
-      lspconfig.sqlls.setup{
-        capabilities = capabilities
-      }
-      lspconfig.basedpyright.setup{
-        capabilities = capabilities
-      }
-      lspconfig.clangd.setup{
-        capabilities = capabilities
-      }
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+            lspconfig.html.setup {
+                capabilities = capabilities
+            }
+            lspconfig.lua_ls.setup {
+                capabilities = capabilities
+            }
+            lspconfig.marksman.setup {
+                capabilities = capabilities
+            }
+            lspconfig.cssls.setup {
+                capabilities = capabilities
+            }
+            lspconfig.ts_ls.setup {
+                capabilities = capabilities
+            }
+            lspconfig.sqlls.setup {
+                capabilities = capabilities
+            }
+            lspconfig.basedpyright.setup {
+                capabilities = capabilities
+            }
+            lspconfig.clangd.setup {
+                capabilities = capabilities
+            }
+            lspconfig.sqlls.setup {
+                capabilities = capabilities
+            }
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
