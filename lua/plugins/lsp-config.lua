26a0f21e7245823155b1a6df2b3de7060b37944f
Wyatt James <wyattsamueljames@gmail.com>
2024-12-12 12:42:13 -0700

    feat(lsp): add rust_analyzer

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -28,6 +28,9 @@ return {
             lspconfig.lua_ls.setup {
                 capabilities = capabilities
             }
+            lspconfig.rust_analyzer.setup {
+                capabilities = capabilities
+            }
             lspconfig.marksman.setup {
                 capabilities = capabilities
             }
