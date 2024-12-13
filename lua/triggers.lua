60dd9b3837ad39549ffd73455bfe5047c2c061e8
Wyatt James <wyattsamueljames@gmail.com>
2024-12-13 07:47:02 -0700

    feat(triggers): add auto rust format

---
+++ lua/triggers.lua
@@ -0,0 +1,10 @@
+local create_autocmd = vim.api.nvim_create_autocmd;
+
+vim.api.nvim_create_augroup("FileExtensionCommands", { clear = true })
+
+-- Format rust file on save, leverages Rust Fmt
+create_autocmd("BufWritePost", {
+    pattern = "*.rs",
+    command = "RustFmt",
+    group = "FileExtensionCommands",
+})
