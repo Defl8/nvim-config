020f38ec6edf93eb0b26e78ddae509e614d69d9b
Wyatt James <wyattsamueljames@gmail.com>
2025-01-12 17:08:16 -0700

    feat(format): update max lines to be 80

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,19 +1,26 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local conform = require("conform")
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+		local conform_formatter = require("conform.formatters")
 
-        conform.setup({
-            formatters_by_ft = {
-                python = { "black" },
-                lua = { "stylua" },
-            },
-            format_on_save = {
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 500
-            }
-        })
-    end
+		conform.setup({
+			formatters_by_ft = {
+				python = { "black" },
+				lua = { "stylua" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+
+		conform_formatter.black = {
+			command = "black",
+			args = { "--line-length", "80" },
+			stdin = true,
+		}
+	end,
 }
