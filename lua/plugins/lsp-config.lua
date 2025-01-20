1df481340e49f4f2eadd0f055138de8af121810c
Wyatt James <wjames4@nait.ca>
2025-01-20 13:52:27 -0700

    feat(lsp): css, html and js

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,77 +1,85 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"basedpyright",
-					"lua_ls",
-					"marksman",
-					"svelte",
-					"rust_analyzer",
-					"biome",
-					"bashls",
-					"html",
-					"phpactor",
-					"emmet_ls",
-				},
-			})
-		end,
-		event = "VeryLazy", -- Loads after critical plugins
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		event = { "BufReadPre", "BufNewFile" }, -- Load when new buffer is created or opened
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "basedpyright",
+                    "lua_ls",
+                    "marksman",
+                    "svelte",
+                    "rust_analyzer",
+                    "biome",
+                    "bashls",
+                    "html",
+                    "phpactor",
+                    "emmet_ls",
+                    "ts_ls",
+                    "cssls",
+                },
+            })
+        end,
+        event = "VeryLazy", -- Loads after critical plugins
+        opts = {
+            auto_install = true,
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" }, -- Load when new buffer is created or opened
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
+            local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.rust_analyzer.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.marksman.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.basedpyright.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.svelte.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.biome.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.bashls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				filetypes = { "html" },
-				capabilities = capabilities,
-			})
-			lspconfig.emmet_ls.setup({
-				filetypes = { "php" },
-				capabilities = capabilities,
-			})
-			lspconfig.phpactor.setup({
-				capabilities = capabilities,
-			})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.rust_analyzer.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.marksman.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.basedpyright.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.svelte.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.biome.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.bashls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.html.setup({
+                filetypes = { "html" },
+                capabilities = capabilities,
+            })
+            lspconfig.emmet_ls.setup({
+                filetypes = { "php", "html" },
+                capabilities = capabilities,
+            })
+            lspconfig.phpactor.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.ts_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.cssls.setup({
+                capabilities = capabilities,
+            })
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
