80d660e6f8eae24bab97dbba9b3aa0d35bb35fa6
Wyatt James <wyattsamueljames@gmail.com>
2025-03-31 07:43:11 -0600

    feat(telescope): add harpoon extensions if I ever want to use it

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,25 +1,27 @@
 return {
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    lazy = true,
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-        { "<leader>ff", "<CMD>Telescope find_files hidden=true<CR>"},
-        { "<leader>fg", "<CMD>Telescope live_grep<CR>" },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        lazy = true
     },
-    tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
+    {
+        "nvim-telescope/telescope.nvim",
+        lazy = true,
+        keys = {
+            { "<leader>ff", "<CMD>Telescope find_files hidden=true<CR>" },
+            { "<leader>fg", "<CMD>Telescope live_grep<CR>" },
         },
-      })
-      require("telescope").load_extension("ui-select")
-    end,
-  },
+        tag = "0.1.5",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("telescope").load_extension("ui-select")
+            --require("telescope").load_extension("harpoon") -- Use telescope for harpoon
+            require("telescope").setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+            })
+        end,
+    },
 }
