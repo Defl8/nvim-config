d5aaf61662319f9cc211e7d8a970a920a9b0210d
Wyatt James <wyattsamueljames@gmail.com>
2025-06-04 17:04:38 -0600

    feat: move to singular color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,58 +1,12 @@
-local function catppuccin(flavour)
-    -- flavour options: mocha, frappe
-    local catppuccin_config = {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup({
-                flavour = flavour,
-                background = { dark = "mocha" },
-                no_italic = true,
-                mason = true
-            })
-            vim.cmd.colorscheme("catppuccin")
-        end
-    }
-    return catppuccin_config
-end
-
-local function rose_pine(variant)
-    local rose_pine_config = {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        priority = 1000,
-        config = function()
-            require("rose-pine").setup({
-                variant = variant,
-                disable_italics = true
-            })
-            vim.cmd.colorscheme("rose-pine")
-        end
-    }
-    return rose_pine_config
-end
-
-local function night_fox(variant)
-    local night_fox_config = {
-        "EdenEast/nightfox.nvim",
-        name = "nightfox",
-        priority = 1000,
-        config = function()
-            require("nightfox").setup({
-                disable_italics = true
-            })
-            vim.cmd.colorscheme(variant)
-        end
-    }
-    return night_fox_config
-end
-
--- CHANGE THIS ONLY
-local colors = { catppuccin("mocha"), rose_pine("moon"), night_fox("carbonfox") }
-math.randomseed(os.time())
-local rand_index = math.random(#colors)
-
 return {
-    rose_pine("moon")
+    "rose-pine/neovim",
+    name = "rose-pine",
+    priority = 1000,
+    config = function()
+        require("rose-pine").setup({
+            variant = "moon",
+            disable_italics = true
+        })
+        vim.cmd.colorscheme("rose-pine")
+    end
 }
