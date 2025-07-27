11cb2009f114bfa8546477a6c3a4746bf601ac11
Wyatt James <wyattsamueljames@gmail.com>
2025-07-27 10:14:54 -0600

    feat: add options

---
+++ lua/options.lua
@@ -0,0 +1,10 @@
+-- Options
+vim.o.relativenumber = true -- Relative line numbers
+vim.o.number = true -- Line numbers
+vim.o.wrap = false -- No line wrapping
+vim.o.swapfile = false -- No swap file for nvim instance
+vim.o.ts = 4 -- # chars to tab over by pressing tab
+vim.o.softtabstop = 4 -- # chars to tab when using TAB key instead of inserting TAB char
+vim.o.shiftwidth = 4 -- Chars to shift when using ">>"
+vim.o.si = true -- Smart indentation on newlines in code blocks
+vim.o.colorcolumn = "80" -- Column for personal max line length
