1f58b3891874a285f885193002beef2c6bcf5d58
Wyatt James <wyattsamueljames@gmail.com>
2025-07-28 14:01:05 -0600

    feat: add lsp maps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,3 +3,7 @@ require "mason".setup()
 vim.lsp.enable({ "lua_ls", "basedpyright" })
 
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { desc = "Format the current buffer" })
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Check inline error" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Execute code action" })
+vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Get all references" })
