8f9332227bcafea7e1fb358e278b7d59bf15a2bd
Wyatt James <wyattsamueljames@gmail.com>
2025-07-29 14:23:44 -0600

    feat: remove default keymaps from oil and add hidden file finding

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,7 @@
 require "oil".setup({
+	use_default_keymaps = true,
 	view_options = {
 		show_hidden = true,
-	}
+	},
 })
 vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent dir" })
