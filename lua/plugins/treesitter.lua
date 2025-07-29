6bb7f327796d12bdd92f01dee18f2ecdb6c7b0e2
Wyatt James <wyattsamueljames@gmail.com>
2025-07-29 08:17:45 -0600

    feat: add bash to treesitter ensure installed list

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,9 @@
 require "nvim-treesitter.configs".setup({
-	ensure_installed = { "lua", "vim", "markdown", "go" },
+	ensure_installed = { "lua", "vim", "markdown", "go", "bash" },
 	auto_install = true,
+	indent = { enable = true },
 	highlight = {
 		enable = true,
 		additional_vim_regex_highlighting = true,
 	},
-	indent = { enable = true },
 })
