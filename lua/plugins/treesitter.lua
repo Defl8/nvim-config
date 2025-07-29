637f2756cf3155398b8cedb2cca6faf81f2536ef
Wyatt James <wyattsamueljames@gmail.com>
2025-07-29 08:20:20 -0600

    feat: add python to treesitter parser

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require "nvim-treesitter.configs".setup({
-	ensure_installed = { "lua", "vim", "markdown", "go", "bash" },
+	ensure_installed = { "lua", "vim", "markdown", "go", "bash", "python" },
 	auto_install = true,
 	indent = { enable = true },
 	highlight = {
