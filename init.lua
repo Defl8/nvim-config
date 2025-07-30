8f4b48dedc76271116f4859ce7b0773c78d77445
Wyatt James <wyattsamueljames@gmail.com>
2025-07-30 07:51:10 -0600

    chore: update comments

--- init.lua
+++ init.lua
@@ -3,10 +3,10 @@ require "keymaps"
 require "pack" -- Plugins
 
 -- Plugins
-require "plugins.oil"
+require "plugins.oil"         -- File browser
 require "plugins.colorscheme" -- Rose Pine Moon
-require "plugins.completions" -- Auto complete via blink.cmp
-require "plugins.lsp"
+require "plugins.completions" -- Auto complete via native omnifunc
+require "plugins.lsp"         -- LSP leveraging nvim-lspconfig configurations
 require "plugins.treesitter"  -- Syntax Highlighting
 require "plugins.harpoon"     -- Fast switching between working files
 require "plugins.telescope"   -- Fuzzy finding
