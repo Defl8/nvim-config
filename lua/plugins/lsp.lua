94f287e07132a4045b8b2ec9b044992d65bc4cb1
Wyatt James <wyattsamueljames@gmail.com>
2025-08-01 12:16:20 -0600

    feat: add configs for some lsp servers and update oil binds

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,10 +1,37 @@
 require "mason".setup()
 
-local servers = { "lua_ls", "gopls", "bashls", "pyright", "black" }
+-- Servers
+local lspconfig = require "lspconfig"
+local servers = { "lua_ls", "gopls", "bashls", "pyright", "templ", "html", "htmx", "emmet_ls", "marksman" }
+-- Server configs
+-- HTML
+lspconfig.html.setup({
+	filetypes = { "html" }
+})
+
+lspconfig.htmx.setup({
+	filetypes = { "html" }
+})
+
+lspconfig.marksman.setup({
+	filetypes = { "markdown" }
+})
+
+
+
 vim.lsp.enable(servers)
 
+-- Keymaps for lsp
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { desc = "Format the current buffer" })
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Check inline error" })
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Execute code action" })
 vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Get all references" })
+
+-- Format on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*.*" },
+	callback = function()
+		vim.lsp.buf.format({ async = false })
+	end
+})
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,7 +1,10 @@
 require "oil".setup({
-	use_default_keymaps = true,
+	use_default_keymaps = false,
 	view_options = {
 		show_hidden = true,
+	},
+	keymaps = {
+		["<CR>"] = "actions.select"
 	},
 })
 vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent dir" })
