f8c27fffaa035f7e20c9587a49aecf2807c87279
Wyatt James <wyattsamueljames@gmail.com>
2025-08-05 22:15:09 -0600

    feat: add blink and add smart server config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -5,13 +5,29 @@ return {
 			ensure_installed = { "lua_ls", "basedpyright", "gopls" },
 		},
 		dependencies = {
-			{ "mason-org/mason.nvim", opts = {}},
+			{ "mason-org/mason.nvim", opts = {} },
 			"neovim/nvim-lspconfig",
 		},
-		config = function()
-			require "mason-lspconfig".setup()
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
-			vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
-		end
 	},
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre" },
+		config = function()
+			local servers = require "mason-registry".get_installed_packages()
+			local map_names = require "mason-lspconfig".get_mappings().package_to_lspconfig
+			local lspconfig = require "lspconfig"
+
+			for _, server in ipairs(servers) do
+				local capabilities = require "blink.cmp".get_lsp_capabilities()
+				local lsp_name = map_names[server.name]
+				lspconfig[lsp_name].setup({
+					capabilties = capabilities
+				})
+			end
+
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+			vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
+			vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
+		end,
+	}
 }
