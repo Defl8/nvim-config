4d6af8b9fbf60912afdad1a984ef69fb822b441e
Wyatt James <wyattsamueljames@gmail.com>
2025-08-12 12:23:19 -0600

    feat: add picker for colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,28 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		require "rose-pine".setup({
-			variant = "moon",
-			styles = {
-				bold = false,
-				italic = false,
-			}
-		})
-		vim.cmd("colorscheme rose-pine")
-	end,
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require "rose-pine".setup({
+				variant = "moon",
+				styles = {
+					bold = false,
+					italic = false,
+				}
+			})
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		config = function()
+			require "catppuccin".setup({
+				flavour = "mocha", -- latte, frappe, macchiato, mocha
+				show_end_of_buffer = true,
+				no_italic = true,
+				no_bold = true,
+				no_underline = true,
+			})
+		end
+	}
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,5 +14,21 @@ return {
 		vim.keymap.set("n", "<leader>ff", builtin.find_files)
 		vim.keymap.set("n", "<leader>rg", builtin.live_grep)
 		vim.keymap.set("n", "<leader>fg", builtin.git_files)
+		vim.keymap.set("n", "<leader>cf", function()
+			builtin.colorscheme({
+				enable_preview = true,
+				attach_mappings = function (_, map)
+					map({"i", "n"}, "<CR>", function(prompt_bufnr)
+						local selection = require("telescope.actions.state").get_selected_entry()
+						require("telescope.actions").close(prompt_bufnr)
+						if selection then
+							vim.cmd.colorscheme(selection.value)
+						end
+					end)
+					return true
+				end
+			})
+		end)
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags)
 	end,
 }
