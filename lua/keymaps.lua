bd07e3f6de8eebb6c5ca0ad302ba4b3408b2bb92
Wyatt James <wyattsamueljames@gmail.com>
2025-11-12 07:40:59 -0700

    feat: add lazy plugin update bind

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,7 +5,7 @@ vim.g.mapleader = " "                        -- Set the leader for keymaps
 
 keymap("n", "<C-d>", "<C-d>zz")              -- Center cursor on page down
 keymap("n", "<C-u>", "<C-u>zz")              -- Center cursor on page up
-keymap("n", "G", "Gzz")              -- Center cursor on page up
+keymap("n", "G", "Gzz")                      -- Center cursor on page up
 keymap("n", "n", "nzzzv")                    -- Next searched word center cursor
 keymap("n", "N", "Nzzzv")                    -- Previous searched word center cusor
 keymap("n", "<leader>c", "_<C-v>")           -- First char in line then visual block
@@ -14,6 +14,9 @@ keymap({ "n", "v" }, "<leader>d", '"+d<CR>') -- Delete to sys clipboard
 keymap({ "n", "v" }, "<leader>p", '"+p')     -- Paste from sys clipboard
 keymap({ "n", "v" }, "<leader>P", '"+P')     -- Paste from sys clipboard
 keymap("n", "gV", "`[v`]")                   -- Last highlighted item
+keymap("n", "<leader>u", function()          -- Update plugins
+	cmd("Lazy sync")
+end)
 
 -- Remove highlight from file
 keymap("n", "<leader>rh", function()
